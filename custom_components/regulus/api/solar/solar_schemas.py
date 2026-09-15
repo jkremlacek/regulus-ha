4c39b232ffa49171e0d48389f8561fd3666c30b8
Jakub Kremláček <jakub@kremlacek.cz>
2026-09-15 11:38:26 +0200

    feat: indicate enabled and disabled states

--- custom_components/regulus/api/solar/solar_schemas.py
+++ custom_components/regulus/api/solar/solar_schemas.py
@@ -8,14 +8,17 @@ class SolarResponseSchema(BaseModel):
     solarPumpRunning: DeviceSchema
     solarCollectorTemperature: DeviceSchema
     solarConsumer1Heating: DeviceSchema
+    solarConsumer1Enabled: DeviceSchema
     solarConsumer1Temperature: DeviceSchema
     solarConsumer1Demand: DeviceSchema
     solarConsumer1MaximumTemperature: DeviceSchema
     solarConsumer2Heating: DeviceSchema
+    solarConsumer2Enabled: DeviceSchema
     solarConsumer2Temperature: DeviceSchema
     solarConsumer2Demand: DeviceSchema
     solarConsumer2MaximumTemperature: DeviceSchema
     solarConsumer3Heating: DeviceSchema
+    solarConsumer3Enabled: DeviceSchema
     solarConsumer3Temperature: DeviceSchema
     solarConsumer3Demand: DeviceSchema
     solarConsumer3MaximumTemperature: DeviceSchema
